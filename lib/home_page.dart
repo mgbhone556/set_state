0c2c65c4a657624a9a932623a859ae6d876f424f
mgbhone556 <mgbhone556@gmail.com>
2026-03-25 10:57:34 +0630

    color and number change

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class SetStateHomePage extends StatefulWidget {
   const SetStateHomePage({super.key, required this.title});
@@ -11,6 +12,24 @@ class SetStateHomePage extends StatefulWidget {
 
 class _HomePageState extends State<SetStateHomePage> {
   int _counter = 0;
+  Color _backgroundColor = Colors.white;
+
+  void _changeColor() {
+    setState(() {
+      _backgroundColor = Color.fromARGB(
+        255,
+        Random().nextInt(256),
+        Random().nextInt(256),
+        Random().nextInt(256),
+      );
+    });
+  }
+
+  void _decreaseCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -21,6 +40,7 @@ class _HomePageState extends State<SetStateHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: _backgroundColor,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
@@ -30,18 +50,43 @@ class _HomePageState extends State<SetStateHomePage> {
         child: Column(
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text(
+              'Press the button to change background!',
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _changeColor,
+              child: const Text("Change Color"),
+            ),
+            const SizedBox(height: 40),
+            const Text('This is the sample fo the setState'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: _decreaseCounter,
+                  tooltip: 'Decrease',
+                  heroTag: "btn1",
+                  child: const Icon(Icons.remove),
+                ),
+                const SizedBox(width: 20),
+                FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  tooltip: 'Increment',
+                  heroTag: "btn2",
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
